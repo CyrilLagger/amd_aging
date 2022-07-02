b705b305103335fa727aeb53bcaa608b47fe2c36
Cyril Lagger <lagger.cyril@gmail.com>
2022-07-02 20:08:35 +0000

    Add setup script

---
+++ A0_setup.R
@@ -0,0 +1,33 @@
+####################################################
+##
+## Project: amd_aging
+##
+## D.Dhirachaikulpanich@liverpool.ac.uk
+## cyril.lagger@liverpool.ac.uk
+##
+####################################################
+##
+
+## Libraries ####
+
+library(Seurat)
+library(scDiffCom)
+library(CellChat)
+library(data.table)
+library(ggplot2)
+library(future)
+library(ComplexUpset)
+library(escape)
+library(ggVennDiagram)
+library(ggExtra)
+library(limma)
+library(ggpubr)
+
+## Options ####
+
+plan(multisession, workers = 20)
+
+## Directory paths ####
+
+path_data <- "data/"
+path_results <- "results/"
